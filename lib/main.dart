7914635e429cdaa6bf0c55c9cb1990bf31ffbe46
luqman jaffary <luqmanjaffary79@gmail.com>
2025-03-17 03:00:51 +0500

    updated code

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,26 @@
+import 'package:app/user_profile.dart';
+// import 'package:firstapp/user_profile.dart';
 import 'package:flutter/material.dart';
+// import 'package:app/second_screen.dart';
+
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('My First Flutter App')),
-        body: Center(
-          child: Text('Hello, Flutter!', style: TextStyle(fontSize: 24)),
-        ),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
+      home: UserProfile(),
     );
   }
 }
-
